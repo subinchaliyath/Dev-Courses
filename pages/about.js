61da18f2e83da3a04aae693d2c4932f5c0d3e92f
subin <subinchaliyath@gmail.com>
2021-05-10 21:25:04 +0530

    about page added

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const about = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is an app to find latest developer courses</p>
+            <p>version: 1.0.0</p>
+        </div>
+    )
+}
+
+export default about
