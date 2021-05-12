d3f6fe0f8ce6d3cd68a692f9dcac17b96b1267c6
subin <subinchaliyath@gmail.com>
2021-05-12 17:48:31 +0530

    updated home style sheet

--- pages/courses/index.js
+++ pages/courses/index.js
@@ -1,10 +1,26 @@
 import Layout from '@/components/Layout'
+import CourseItem from '@/components/CourseItem'
+import { API_URL } from '@/config/index'
 
+export default function CoursePage({ courses }) {
+  return (
+    <Layout>
+      <h1>Courses</h1>
+      {courses.length === 0 && <h3>No courses to show</h3>}
 
-export default function index() {
-    return (
-        <Layout>
-            <h1>All Courses</h1>
-        </Layout>
-    )
+      {courses.map((cours) => (
+        <CourseItem key={cours.id} cours={cours} />
+      ))}
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/courses`)
+  const courses = await res.json()
+
+  return {
+    props: { courses },
+    revalidate: 1,
+  }
 }
