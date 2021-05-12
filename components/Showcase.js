dfd75e1a10cdd710d6869be1d3d04dbd0a37d691
subin <subinchaliyath@gmail.com>
2021-05-12 17:43:54 +0530

    added showcase component

---
+++ components/Showcase.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Showcase.module.css'
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome To Course Hub!</h1>
+      <h2>Find the latest Dev course</h2>
+    </div>
+  )
+}
