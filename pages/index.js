953cfbfaf6e1d2a0629165af38cb3f9b194df4c0
subin <subinchaliyath@gmail.com>
2021-05-12 17:49:06 +0530

    updated config file with env

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,33 @@
+import Link from 'next/link'
 import Layout from '@/components/Layout'
+import CourseItem from '@/components/CourseItem'
+import { API_URL } from '@/config/index'
 
-export default function HomePage() {
+export default function HomePage({ courses }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Courses</h1>
+      {courses.length === 0 && <h3>No courses to show</h3>}
+
+      {courses.map((cours) => (
+        <CourseItem key={cours.id} cours={cours} />
+      ))}
+
+      {courses.length > 0 && (
+        <Link href='/courses'>
+          <a className='btn-secondary'>View All courses</a>
+        </Link>
+      )}
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/courses`)
+  const courses = await res.json()
+
+  return {
+    props: { courses: courses.slice(0, 3) },
+    revalidate: 1,
+  }
 }
