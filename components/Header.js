c39b40f37d87b0648e296b008b223a548cfbc428
subin <subinchaliyath@gmail.com>
2021-05-16 13:03:45 +0530

    search functionality

--- components/Header.js
+++ components/Header.js
@@ -1,24 +1,31 @@
-import Link from 'next/link'
-import styles from '@/styles/Header.module.css'
+import Link from "next/link";
+import styles from "@/styles/Header.module.css";
+import Search from "./Search";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <Link href='/'>
+        <Link href="/">
           <a>Dev Courses</a>
         </Link>
       </div>
+      <Search />
 
       <nav>
         <ul>
           <li>
-            <Link href='/courses'>
+            <Link href="/courses">
               <a>Courses</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/courses/add">
+              <a>Add Course</a>
             </Link>
           </li>
         </ul>
       </nav>
     </header>
-  )
+  );
 }
--- pages/courses/[slug].js
+++ pages/courses/[slug].js
@@ -1,15 +1,30 @@
-import { FaPencilAlt, FaTimes } from 'react-icons/fa'
-import Link from 'next/link'
-import Image from 'next/image'
-import Layout from '@/components/Layout'
-import { API_URL } from '@/config/index'
-import styles from '@/styles/Course.module.css'
-import {dateFormatLong} from '@/utils/dateFormat.js'
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
+import Link from "next/link";
+import Image from "next/image";
+import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
+import styles from "@/styles/Course.module.css";
+import { dateFormatLong } from "@/utils/dateFormat.js";
+import { useRouter } from "next/router";
 
 export default function CoursePage({ cours }) {
-  const deleteCourse = (e) => {
-    console.log('delete')
-  }
+  const router = useRouter();
+
+  const deleteCourse = async (e) => {
+    if (confirm("Are you sure?")) {
+      const res = await fetch(`${API_URL}/courses/${cours.id}`, {
+        method: "DELETE",
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        toast.error(data.message);
+      } else {
+        router.push("/courses");
+      }
+    }
+  };
 
   return (
     <Layout>
@@ -20,12 +35,12 @@ export default function CoursePage({ cours }) {
               <FaPencilAlt /> Edit Course
             </a>
           </Link>
-          <a href='#' className={styles.delete} onClick={deleteCourse}>
+          <a href="#" className={styles.delete} onClick={deleteCourse}>
             <FaTimes /> Delete Course
           </a>
         </div>
 
-        <span> 
+        <span>
           {dateFormatLong(cours.date)} at {cours.time}
         </span>
         <h1>{cours.name}</h1>
@@ -40,38 +55,38 @@ export default function CoursePage({ cours }) {
         <h3>Description:</h3>
         <p>{cours.description}</p>
 
-        <Link href='/courses'>
-          <a className={styles.back}>{'<'} Go Back</a>
+        <Link href="/courses">
+          <a className={styles.back}>{"<"} Go Back</a>
         </Link>
       </div>
     </Layout>
-  )
+  );
 }
 
 export async function getStaticPaths() {
-  const res = await fetch(`${API_URL}/courses`)
-  const courses = await res.json()
+  const res = await fetch(`${API_URL}/courses`);
+  const courses = await res.json();
 
   const paths = courses.map((cours) => ({
     params: { slug: cours.slug },
-  }))
+  }));
 
   return {
     paths,
     fallback: true,
-  }
+  };
 }
 
 export async function getStaticProps({ params: { slug } }) {
-  const res = await fetch(`${API_URL}/courses/?slug=${slug}`)
-  const courses = await res.json()
+  const res = await fetch(`${API_URL}/courses/?slug=${slug}`);
+  const courses = await res.json();
 
   return {
     props: {
       cours: courses[0],
     },
     revalidate: 1,
-  }
+  };
 }
 
 // export async function getServerSideProps({ query: { slug } }) {
